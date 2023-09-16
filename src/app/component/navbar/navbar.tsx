b0006b5096cf93f14c0ce63d8789117947105196
GuhSntLean <guleandro.66@gmail.com>
2023-09-16 00:48:19 -0300

    ajuste no navbar inicial

--- src/app/component/navbar/navbar.tsx
+++ src/app/component/navbar/navbar.tsx
@@ -1,10 +1,11 @@
 import './navbar.css';
 
+
 function NavBar() {
   return (
     <nav className="navbar">
-      <div className="personalinformation">
-        <img src="" alt='my-personal-photo' />
+      <div className="personal-information">
+        <img src="/image/personalphoto.jpg" className='person-photo' alt='my-personal-photo' />
         <p className="name">Gustavo S. Leandro</p>
       </div>
     </nav>
