31a119efabb1bbe5913eb6759d7a15224dbf99fb
GuhSntLean <guleandro.66@gmail.com>
2023-09-16 20:45:02 -0300

    criação de componente footer para desenvolvimento e ajuste no componente navbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
+import Footer from "./component/footer/footer";
 import NavBar from "./component/navbar/navbar"
 
 export default function Home() {
   return (
     <div className="container">
       <NavBar />
+      <Footer />
     </div>
   );
 }
