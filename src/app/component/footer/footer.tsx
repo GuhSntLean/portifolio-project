a1e65db71f5a3d7172a9ecd108257268bedaf817
GuhSntLean <guleandro.66@gmail.com>
2023-09-20 23:28:17 -0300

    refatoracão do footer e criação de novo visual

--- src/app/component/footer/footer.tsx
+++ src/app/component/footer/footer.tsx
@@ -3,7 +3,28 @@ import "./footer.css";
 function Footer() {
   return (
     <footer className="footer">
-
+      <div className="options-footer">
+        <ul>
+          <li>
+            <a href="#">Home</a>
+          </li>
+          <li>
+            <a href="#">Sobre</a>
+          </li>
+          <li>
+            <a href="#">Formação</a>
+          </li>
+          <li>
+            <a href="#">Projetos</a>
+          </li>
+          <li>
+            <a href="#">Contato</a>
+          </li>
+        </ul>
+      </div>
+      <div className="information-licence">
+        <p> © Gustavo S. Leandro </p>
+      </div>
     </footer>
   );
 }
